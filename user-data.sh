7703eb05083040172e5d1ba83ad5934c6ce5e673
llijd <m17377797887@163.com>
2025-12-01 17:00:15 +0800

    Create user-data.sh

---
+++ user-data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -v
+# 挂盘到/disk1
+cat >> /root/InitDataDisk.sh << "EOF"
+#!/bin/bash
+echo "p
+n
+p
+w
+" |  fdisk -u /dev/vdb
+EOF
+/bin/bash /root/InitDataDisk.sh
+rm -f /root/InitDataDisk.sh
+mkfs -t ext4 /dev/vdb1
+cp /etc/fstab /etc/fstab.bak
+mkdir /disk1
+echo `blkid /dev/vdb1 | awk '{print $2}' | sed 's/\\\"//g'` /disk1 ext4 defaults 0 0 >> /etc/fstab
+mount -a
+# 这里配置安装脚本
+yum install -y nginx
+# 配置启动脚本
+/usr/sbin/nginx
